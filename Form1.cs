caf5ad589b328d25986816bf724fc2f905825f0a
nadiramlha <nadiraaamilhanf@gmail.com>
2023-09-06 12:06:25 +0700

    first commit

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 + nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 * nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 - nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int nilai1 = int.Parse(textBox1.Text);
+            int nilai2 = int.Parse(textBox2.Text);
+            int hasil;
+            hasil = nilai1 / nilai2;
+            textBox3.Text = hasil.ToString();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
